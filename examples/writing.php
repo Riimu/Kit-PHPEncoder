7f46aed6fd6c33df88e449bcd699cf0b8d3d8229
Riikka Kalliomäki <riikka.kalliomaki@gmail.com>
2013-11-01 16:43:00 +0200

    Minor example fix

--- examples/writing.php
+++ examples/writing.php
@@ -6,6 +6,7 @@ spl_autoload_register();
 $encoder = new \Riimu\Kit\PHPEncoder\PHPEncoder();
 echo $encoder->encode(['foo' => 'bar', [1, true, false, null, 1.1]]);
 
+echo PHP_EOL;
+
 $encoder->setIndent(false);
-//echo $encoder->encode(['foo' => 'bar', [1, true, false, null, 1.1]]);
-echo $encoder->encode([1=>'b',0=>'a']);
+echo $encoder->encode(['foo' => 'bar', [1, true, false, null, 1.1]]);
