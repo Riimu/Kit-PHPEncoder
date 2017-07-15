858d4be7b6de0e3cdd62861ed2ee6d23cfa2d76c
Riikka Kalliomäki <riikka.kalliomaki@gmail.com>
2017-07-15 16:21:38 +0300

    Update documentation

--- examples/writing.php
+++ examples/writing.php
@@ -3,7 +3,7 @@
 /* This simple example demonstrates how to encode a simple array using default
    settings and while using minimal whitespace */
 
-require __DIR__ . '/../src/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $encoder = new \Riimu\Kit\PHPEncoder\PHPEncoder();
 echo $encoder->encode(['foo' => 'bar', [1, true, false, null, 1.0]]);
